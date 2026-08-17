8ebac2527225bf69adf08116c6c3e6989e15109b
Urtzivi <urtzi@suibroker.es>
2026-08-17 09:19:27 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='runflow-pilot-2';
+const CACHE='runflow-pilot-3';
 const CORE=['/','/login','/coach','/athlete','/css/app.css','/js/login.js','/js/coach.js','/js/athlete.js','/assets/icon-192.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
